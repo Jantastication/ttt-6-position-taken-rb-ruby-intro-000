c597bee1456fef33f328d1ffd0cd4dcc4068df9c
Jantastication O <georginaoregx@gmail.com>
2018-09-20 04:48:10 +0000

    automatically backed up by learn

--- lib/position_taken.rb
+++ lib/position_taken.rb
@@ -2,4 +2,14 @@
 board = [" ", " ", " ", " ", " ", " ", " ", " ", " "]
 def position_taken?(board, index)
   board[index]
+  if board == " " and index == 0
+    false
+  elsif board == "" and index == 0
+    false
+  elsif board == nil and index ==0
+    false
+  elsif board[index] == "X" || board[index] == "O"
+    true
+  end
 end
+
